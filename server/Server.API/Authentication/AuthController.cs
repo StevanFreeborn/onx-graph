6d83e7da0bed795f86bf78c649244c0e6d20c119
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-03 20:04:45 -0600

    chore: add psuedo code to register action

--- server/Server.API/Authentication/AuthController.cs
+++ server/Server.API/Authentication/AuthController.cs
@@ -7,6 +7,26 @@ static class AuthController
 {
   internal static Task<IResult> Register()
   {
+    // in order to register a user we need:
+    // - request with user data
+    //   - email
+    //   - password
+
+    // need to validate the request
+    // - email is valid
+    // - password is valid
+
+    // need to check if the user already exists
+    // - if the user exists, return an error
+
+    // need to generate a unique username
+    // - email username + random number
+
+    // need to hash the password
+    // - bcrypt
+
+    // need to save the user to the database
+
     throw new NotImplementedException();
   }
 
