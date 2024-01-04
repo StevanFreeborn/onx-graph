6be275e5fd69f03937b4db2dc37382087a47aa8a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-03 18:59:29 -0600

    chore: stub out auth controller actions

---
+++ server/Server.API/Authentication/AuthController.cs
@@ -0,0 +1,27 @@
+namespace Server.API.Authentication;
+
+/// <summary>
+/// Controller for handling authentication requests
+/// </summary>
+static class AuthController
+{
+  internal static Task<IResult> RegisterAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  internal static Task<IResult> LoginAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  internal static Task<IResult> LogoutAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  internal static Task<IResult> RefreshTokenAsync()
+  {
+    throw new NotImplementedException();
+  }
+}
