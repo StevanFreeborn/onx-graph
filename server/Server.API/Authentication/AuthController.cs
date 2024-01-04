8662f051cfa53ccaa0ee830c633e35cee048cf01
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-03 19:01:25 -0600

    refactor: remove async suffix from action names

--- server/Server.API/Authentication/AuthController.cs
+++ server/Server.API/Authentication/AuthController.cs
@@ -5,22 +5,22 @@ namespace Server.API.Authentication;
 /// </summary>
 static class AuthController
 {
-  internal static Task<IResult> RegisterAsync()
+  internal static Task<IResult> Register()
   {
     throw new NotImplementedException();
   }
 
-  internal static Task<IResult> LoginAsync()
+  internal static Task<IResult> Login()
   {
     throw new NotImplementedException();
   }
 
-  internal static Task<IResult> LogoutAsync()
+  internal static Task<IResult> Logout()
   {
     throw new NotImplementedException();
   }
 
-  internal static Task<IResult> RefreshTokenAsync()
+  internal static Task<IResult> RefreshToken()
   {
     throw new NotImplementedException();
   }
--- server/Server.API/Authentication/AuthRoutes.cs
+++ server/Server.API/Authentication/AuthRoutes.cs
@@ -13,23 +13,23 @@ static class AuthRoutes
     var group = app.MapGroup("auth");
 
     group
-      .MapPost("register", AuthController.RegisterAsync)
+      .MapPost("register", AuthController.Register)
       .WithName("RegisterUser")
       .WithDescription("Register a new user");
 
     group
-      .MapPost("login", AuthController.LoginAsync)
+      .MapPost("login", AuthController.Login)
       .WithName("LoginUser")
       .WithDescription("Login a user");
 
     group
-      .MapPost("logout", AuthController.LogoutAsync)
+      .MapPost("logout", AuthController.Logout)
       .RequireAuthorization()
       .WithName("LogoutUser")
       .WithDescription("Logout a user");
 
     group
-      .MapPost("refresh-token", AuthController.RefreshTokenAsync)
+      .MapPost("refresh-token", AuthController.RefreshToken)
       .WithName("RefreshToken")
       .WithDescription("Refresh a user's token");
   }
