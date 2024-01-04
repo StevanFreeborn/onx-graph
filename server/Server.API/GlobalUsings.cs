f9d1f273e7d4ef9a5b4ad4faab74aeead844a723
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-03 18:52:44 -0600

    feat: wire up jwt authentication and authorization

--- server/Server.API/GlobalUsings.cs
+++ server/Server.API/GlobalUsings.cs
@@ -1,4 +1,11 @@
+global using System.Text;
+
 global using Asp.Versioning;
 
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.Extensions.Options;
+global using Microsoft.IdentityModel.Tokens;
+
 global using Serilog;
-global using Serilog.Events;
+
+global using Server.API.Authentication;
--- server/Server.API/Program.cs
+++ server/Server.API/Program.cs
@@ -1,12 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
+
+// add logging
 builder.Host.UseSerilog(
   (context, loggerConfiguration) => loggerConfiguration
     .ReadFrom.Configuration(config)
     .Enrich.FromLogContext()
 );
 
+
+// add versioning
 var versionOne = new ApiVersion(1, 0);
 
 builder.Services.AddApiVersioning(opts =>
@@ -20,22 +24,61 @@ builder.Services.AddApiVersioning(opts =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// add authentication and authorization
+var jwtOptions = new JwtOptions();
+config.GetSection(nameof(JwtOptions)).Bind(jwtOptions);
+
+builder.Services.ConfigureOptions<JwtOptionsSetup>();
+
+builder.Services
+  .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(
+    o => o.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidIssuer = jwtOptions.Issuer,
+      ValidAudience = jwtOptions.Audience,
+      IssuerSigningKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(jwtOptions.Secret)
+      ),
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      ClockSkew = TimeSpan.FromSeconds(0),
+    }
+  );
+
+builder.Services.AddAuthorization();
+
+
+// build the app
 var app = builder.Build();
 
+
+// add request logging middleware
 app.UseSerilogRequestLogging();
 
+
+// add swagger when developing
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
 }
 
+
+// add https redirection
 app.UseHttpsRedirection();
 
+
+// build the api version set
 var versionSet = app.NewApiVersionSet()
   .HasApiVersion(new ApiVersion(1, 0))
   .Build();
 
+
+// map endpoints
 app
   .MapGet("/", (ILogger<Program> logger) =>
   {
@@ -49,4 +92,11 @@ app
   .WithApiVersionSet(versionSet)
   .MapToApiVersion(versionOne);
 
+
+// wire up authentication and authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+// run the app
 app.Run();
