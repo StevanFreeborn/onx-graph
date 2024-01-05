9458c097ade926cc944fd6943d864992a056ead5
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-04 22:08:51 -0600

    fix: implement interface

--- server/Server.API/Identity/MongoUserRepository.cs
+++ server/Server.API/Identity/MongoUserRepository.cs
@@ -1,6 +1,22 @@
+
 namespace Server.API.Identity;
 
 class MongoUserRepository(MongoDbContext context) : IUserRepository
 {
   private readonly MongoDbContext _context = context;
+
+  public Task<User> CreateUserAsync(User user)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<User?> GetUserByEmailAsync(string email)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<User?> GetUserByUsernameAsync(string username)
+  {
+    throw new NotImplementedException();
+  }
 }
