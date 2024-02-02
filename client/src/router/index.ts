0adabf680c71d0b2d2d0367c9e5c2c76267691ae
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-02-01 22:22:30 -0600

    feat: delineate public views from authenticated ones

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,28 +1,70 @@
+import { useUserStore } from '@/stores/userStore';
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/public',
+      name: 'public',
+      component: () => import('../components/PublicLayout.vue'),
+      beforeEnter: () => {
+        const { user } = useUserStore();
+
+        return user ? { name: 'graphs' } : true;
+      },
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: () => import('../views/HomeView.vue'),
+        },
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('../views/LoginView.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('../views/RegisterView.vue'),
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue'),
+        },
+      ],
+    },
+    {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue'),
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      name: 'root',
+      redirect: { name: 'graphs' },
+      beforeEnter: () => {
+        const { user } = useUserStore();
+
+        if (user === null) {
+          return { name: 'login' };
+        }
+
+        const isExpired = user.expiresAtInSeconds < Date.now() / 1000;
+
+        if (isExpired) {
+          // TODO: Make request to server to attempt to refresh token
+          // If successful, update user store and continue
+          // if not, post to server to log user out and then
+          // log user out in the client and redirect to login
+        }
+
+        return true;
+      },
+      children: [
+        {
+          path: 'graphs',
+          name: 'graphs',
+          component: () => import('../views/GraphsView.vue'),
+        },
+      ],
     },
   ],
 });
