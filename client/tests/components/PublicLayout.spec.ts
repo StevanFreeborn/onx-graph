1e28f012c34ab9ccda45da6b14f83ff7c21b9187
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-02-28 19:40:14 -0600

    fix: rename render function

--- client/tests/components/PublicLayout.spec.ts
+++ client/tests/components/PublicLayout.spec.ts
@@ -2,27 +2,27 @@ import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/vue';
 import { afterEach, describe, expect, it } from 'vitest';
 import PublicLayout from '../../src/components/PublicLayout.vue';
-import { renderApp } from '../testUtils';
+import { customRender } from '../testUtils';
 
 describe('PublicLayout', () => {
   afterEach(cleanup);
 
   it('should display a logo', async () => {
-    const { getByAltText } = await renderApp(PublicLayout);
+    const { getByAltText } = await customRender(PublicLayout);
     const logo = getByAltText(/logo/);
 
     expect(logo).toBeInTheDocument();
   });
 
   it('should display a login link', async () => {
-    const { getByRole } = await renderApp(PublicLayout);
+    const { getByRole } = await customRender(PublicLayout);
     const loginLink = getByRole('link', { name: 'Login' });
 
     expect(loginLink).toBeInTheDocument();
   });
 
   it('should display a register link', async () => {
-    const { getByRole } = await renderApp(PublicLayout);
+    const { getByRole } = await customRender(PublicLayout);
     const registerLink = getByRole('link', { name: 'Register' });
 
     expect(registerLink).toBeInTheDocument();
