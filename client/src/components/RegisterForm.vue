783bcc327112f1559c622a9a76e217dd40231dd0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-02-29 22:24:17 -0600

    fix: disable interactive elements until mounted and disable button when submitting.

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import { useAuthService } from '@/composables/useAuthService';
+  import { useMounted } from '@/composables/useMounted';
+  import { useSubmitting } from '@/composables/useSubmitting';
   import { toTitleCase } from '@/utils';
   import isEmail from 'validator/es/lib/isEmail';
   import { reactive } from 'vue';
@@ -29,11 +31,14 @@
     errors: [],
   });
 
+  const { isSubmitting, setIsSubmitting } = useSubmitting();
+  const isMounted = useMounted();
   const authService = useAuthService();
   const router = useRouter();
 
   async function handleRegisterFormSubmit() {
     formState.errors = [];
+    setIsSubmitting(true);
 
     const keys = Object.keys(formState.fields) as (keyof RegisterFormState['fields'])[];
 
@@ -61,6 +66,7 @@
     const formStateHasError = Object.values(formState.fields).some(field => field.errorMessage);
 
     if (formStateHasError) {
+      setIsSubmitting(false);
       return;
     }
 
@@ -71,9 +77,11 @@
 
     if (registerResult.err) {
       formState.errors.push(...registerResult.val.map(err => err.message));
+      setIsSubmitting(false);
       return;
     }
 
+    setIsSubmitting(false);
     router.push('/login');
   }
 
@@ -101,6 +109,7 @@
       <div class="form-group">
         <label for="email">Email</label>
         <input
+          :disabled="isMounted === false"
           type="email"
           id="email"
           name="email"
@@ -116,6 +125,7 @@
       <div class="form-group">
         <label for="password">Password</label>
         <input
+          :disabled="isMounted === false"
           type="password"
           id="password"
           name="password"
@@ -131,6 +141,7 @@
       <div class="form-group">
         <label for="confirmPassword">Confirm Password</label>
         <input
+          :disabled="isMounted === false"
           type="password"
           id="confirmPassword"
           name="confirmPassword"
@@ -143,7 +154,9 @@
           {{ formState.fields.confirmPassword.errorMessage }}
         </span>
       </div>
-      <button class="register-button" type="submit">Register</button>
+      <button :disabled="isMounted === false || isSubmitting" class="register-button" type="submit">
+        Register
+      </button>
       <ul class="form-errors">
         <li class="error-message" v-for="error in formState.errors" :key="error">{{ error }}</li>
       </ul>
