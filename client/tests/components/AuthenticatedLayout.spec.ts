1d4f0b88a08234a9da5e3b36afb538d57134557c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-01 23:45:38 -0500

    tests: stub out component tests for authenticated layout

---
+++ client/tests/components/AuthenticatedLayout.spec.ts
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/vue';
+import { describe, afterEach } from 'vitest';
+
+describe('AuthenticatedLayout', () => {
+  afterEach(cleanup);
+});
