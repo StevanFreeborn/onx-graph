e11d0501dcf35fdca69c92b8f787ad9b18b9af2f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-06 13:00:26 -0500

    tests: add integration test for user controller get user method

--- server/Server.API.Tests/Integration/UsersControllerTests.cs
+++ server/Server.API.Tests/Integration/UsersControllerTests.cs
@@ -24,20 +24,40 @@ public class UsersControllerTests(TestServerFactory serverFactory) : Integration
   [Fact]
   public async Task GetUser_WhenCalledAndNotGivenInvalidUserId_ItShouldReturnBadRequest()
   {
+    var (_, user) = FakeDataFactory.TestUser.Generate();
+
+    var userJwtToken = TestJwtTokenBuilder
+      .Create()
+      .WithClaim(new(JwtRegisteredClaimNames.Sub, user.Id))
+      .Build();
+
+    _client.DefaultRequestHeaders.Authorization = new("Bearer", userJwtToken);
+
     var response = await _client.GetAsync("/users/invalid-user-id");
 
     response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+    var responseBody = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+    responseBody!.Errors["UserId"].Should().Contain("Invalid user id");
   }
 
   [Fact]
   public async Task GetUser_WhenCalledByDifferentUser_ItShouldReturnForbidden()
   {
-    var (_, user) = FakeDataFactory.TestUser.Generate();
-    user.IsVerified = true;
+    var (_, userRequesting) = FakeDataFactory.TestUser.Generate();
+    var (_, userRequested) = FakeDataFactory.TestUser.Generate();
 
-    await Context.Users.InsertOneAsync(user);
+    var userRequestingJwtToken = TestJwtTokenBuilder
+      .Create()
+      .WithClaim(new(JwtRegisteredClaimNames.Sub, userRequesting.Id))
+      .Build();
 
-    var response = await _client.GetAsync($"/users/{user.Id}");
+    _client.DefaultRequestHeaders.Authorization = new("Bearer", userRequestingJwtToken);
+
+    await Context.Users.InsertOneAsync(userRequested);
+
+    var response = await _client.GetAsync($"/users/{userRequested.Id}");
 
     response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
   }
@@ -45,9 +65,24 @@ public class UsersControllerTests(TestServerFactory serverFactory) : Integration
   [Fact]
   public async Task GetUser_WhenCalledAndUserDoesNotExist_ItShouldReturnNotFound()
   {
+    var (_, user) = FakeDataFactory.TestUser.Generate();
+
+    var userJwtToken = TestJwtTokenBuilder
+      .Create()
+      .WithClaim(new(JwtRegisteredClaimNames.Sub, user.Id))
+      .Build();
+
+    _client.DefaultRequestHeaders.Authorization = new("Bearer", userJwtToken);
+
     var response = await _client.GetAsync($"/users/{ObjectId.GenerateNewId()}");
 
     response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+    var responseBody = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+    responseBody!.Title.Should().Be("Failed to get user");
+    responseBody!.Detail.Should().Be("Unable to retrieve user. See errors for details.");
+    responseBody!.Extensions.Should().ContainKey("Errors");
   }
 
   [Fact]
@@ -56,10 +91,22 @@ public class UsersControllerTests(TestServerFactory serverFactory) : Integration
     var (_, user) = FakeDataFactory.TestUser.Generate();
     user.IsVerified = true;
 
+    var userJwtToken = TestJwtTokenBuilder
+      .Create()
+      .WithClaim(new(JwtRegisteredClaimNames.Sub, user.Id))
+      .Build();
+
+    _client.DefaultRequestHeaders.Authorization = new("Bearer", userJwtToken);
+
     await Context.Users.InsertOneAsync(user);
 
     var response = await _client.GetAsync($"/users/{user.Id}");
 
     response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var responseBody = await response.Content.ReadFromJsonAsync<UserResponse>();
+
+    responseBody!.Id.Should().Be(user.Id);
+    responseBody!.Username.Should().Be(user.Username);
   }
 }
