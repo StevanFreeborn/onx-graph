dc9d292be88930a69cd81f4013a91be29aec8aac
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-08 11:53:03 -0500

    tests: add tests for AuthenticatedLayout

--- client/tests/components/AuthenticatedLayout.spec.ts
+++ client/tests/components/AuthenticatedLayout.spec.ts
@@ -1,11 +1,45 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/vue';
 import { afterEach, describe, expect, it } from 'vitest';
+import AuthenticatedLayout from '../../src/components/AuthenticatedLayout.vue';
+import { customRender } from '../testUtils';
 
 describe('AuthenticatedLayout', () => {
   afterEach(cleanup);
 
-  it('should render the layout', async () => {
-    expect(true).toBe(true);
+  it('should render the view content within main element', async () => {
+    const { getByText, getByRole } = await customRender(AuthenticatedLayout, {
+      global: {
+        stubs: {
+          RouterView: {
+            template: '<div>view content</div>',
+          },
+        },
+      },
+    });
+
+    const main = getByRole('main');
+    const viewContent = getByText(/view content/);
+
+    expect(main).toBeInTheDocument();
+    expect(viewContent).toBeInTheDocument();
+  });
+
+  it('should render the sidebar in aside element', async () => {
+    const { getByText, getByRole } = await customRender(AuthenticatedLayout, {
+      global: {
+        stubs: {
+          NavigationSidebar: {
+            template: '<div>sidebar</div>',
+          },
+        },
+      },
+    });
+
+    const aside = getByRole('complementary');
+    const sidebar = getByText(/sidebar/);
+
+    expect(aside).toBeInTheDocument();
+    expect(sidebar).toBeInTheDocument();
   });
 });
