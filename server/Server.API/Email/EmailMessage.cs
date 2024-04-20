40ecd75b11f107f636a2a4c16150267eb62c5415
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-20 12:36:00 -0500

    docs: add missing xml comments

--- server/Server.API/Email/EmailMessage.cs
+++ server/Server.API/Email/EmailMessage.cs
@@ -1,6 +1,20 @@
+/// <summary>
+/// Represents an email message
+/// </summary>
 class EmailMessage
 {
+  /// <summary>
+  /// The email address to send the email to
+  /// </summary>
   public string To { get; set; } = string.Empty;
+
+  /// <summary>
+  /// The subject of the email
+  /// </summary>
   public string Subject { get; set; } = string.Empty;
+
+  /// <summary>
+  /// The plain text content of the email
+  /// </summary>
   public string HtmlContent { get; set; } = string.Empty;
 }
